8eccce63a0d21961ce1f3d022abbbd60cbaddb38
AchmadAjiePriyajie <achmadajie74@gmail.com>
2024-12-09 19:09:45 +0700

    dataset change and model change

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import joblib
 
 app = Flask(__name__)
 
-saved_model = joblib.load('modelSVC.joblib')
-saved_tfidf = joblib.load('TF-IDF.joblib')
+saved_model = joblib.load('svm_model.joblib')
+saved_tfidf = joblib.load('tfidf_vectorizer.joblib')
 
 def predictNewData(tweets):
 
@@ -14,8 +14,12 @@ def predictNewData(tweets):
 
     if input_prediction == 1:
         prediction = 'Ujaran Kebencian'
+    elif input_prediction == 2:
+        prediction = 'Kata kasar'
+    elif input_prediction == 3:
+        prediction = 'Kata kasar dan ujaran kebencian'
     else:
-        prediction = 'Bukan Ujaran Kebencian'
+        prediction = 'Kata aman'
 
     return prediction
 
@@ -27,12 +31,22 @@ def labelCSVData(input_file):
     nama_kolom_baru = ['Teks']
     data = data.rename(columns=dict(zip(data.columns, nama_kolom_baru)))
 
+    def map_prediction(value):
+        if value == 1:
+            return 'Ujaran Kebencian'
+        elif value == 2:
+            return 'Kata Kasar'
+        elif value == 3:
+            return 'Kata Kasar dan Ujaran Kebencian'
+        else:
+            return 'Kata Aman'
+
     # Perform prediction and label the data
     data['prediction'] = saved_model.predict(saved_tfidf.transform(data['Teks']))
-    data['prediction'] = data['prediction'].apply(lambda x: 'HS' if x == 1 else 'Non_HS')
+    data['prediction'] = data['prediction'].apply(map_prediction)
 
     # Save the labeled data into a new CSV file
-    labeled_file = 'ujaranKebencianApps.csv'
+    labeled_file = 'dataset_indonesiam_toxic.csv'
     data.to_csv(labeled_file, index=False)
 
     return labeled_file
@@ -60,7 +74,7 @@ def label():
     input_file = request.files['csv_file']
     if input_file and input_file.filename.endswith('.csv'):
         labeled_file = labelCSVData(input_file)
-        return send_file(labeled_file, as_attachment=True, download_name='ujaranKebencianApps.csv')
+        return send_file(labeled_file, as_attachment=True, download_name='dataset_indonesiam_toxic.csv')
     else:
         return render_template("content.html", error_message="Invalid file, please upload a CSV file.")
     
